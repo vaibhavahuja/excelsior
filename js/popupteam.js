f86d293de69d16389300c9fcfb5c3d7cbcc7555f
vaibhavahuja <vaibhavahuja@outlook.com>
2017-02-01 19:11:50 +0530

    team popups
    fixed some minor issues like height of body and shizz

---
+++ js/popupteam.js
@@ -0,0 +1,12 @@
+// Inline popups
+$('.council').magnificPopup({
+  delegate: '.memberpic',
+  removalDelay: 500, //delay removal by X to allow out-animation
+  callbacks: {
+    beforeOpen: function() {
+       this.st.mainClass = this.st.el.attr('data-effect');
+    }
+  },
+  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+});
+
